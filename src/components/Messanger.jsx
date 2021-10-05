fbc38978182a806dccec2a93f653e3932b2ebfd9
Abhishek Verma <77038651+abhi5iet@users.noreply.github.com>
2021-10-05 23:04:20 +0530

    worked on front page

--- src/components/Messanger.jsx
+++ src/components/Messanger.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import Login from './account/Login';
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    height: 200,
+    backgroundColor: '#00bfa5',
+    boxShadow: 'none',
+}));
+const StyledBox = styled(Box)(({ theme }) => ({
+    height: '100vh',
+    backgroundColor: '#DCDCDC',
+}));
 
 export const Messanger = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+        <StyledBox>
+            <StyledAppBar position="static">
+                <Toolbar>
+                </Toolbar>
+            </StyledAppBar>
+            <Login />
+        </StyledBox>
     )
 }
---
+++ src/components/account/Login.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Dialog, List, ListItem, Typography, withStyles } from '@mui/material';
+import { Box } from '@mui/system';
+import { makeStyles } from '@mui/styled-engine';
+
+const style = {
+    dialogg : {
+        height : '95%',
+        width : '60%',
+        marginTop : '12%',
+        boxShadow : 'none',
+        borderRadius: 0,
+        maxHeight : '100%',
+        maxWidth : '100%',
+    }
+}
+
+const useStyles = makeStyles({
+    comp : {
+        display: 'flex',
+    },
+    lftComp : {
+        padding : '56px 0px 56px 56px',
+    },
+    qrCode : {
+        padding : '50px 0px 0px 50px',
+        height : 264,
+        width : 264
+    },
+    title: {
+        marginBottom : 0,
+        fontSize: 26,
+        fontWeight: 300,
+        fontFamily: 'Segoe UI,Helvetica Neue,Helvetica,Lucida Grande,Arial,Ubuntu,Cantarell,Fira Sans,sans-serif',
+        color : '#525252',
+    },
+    listt:{
+        '& > *' : {
+            padding: 0,
+            fontSize: 18,
+            marginTop: 15,
+            color: '#4a4a4a',
+            lineHeight: '28px',
+        }
+    }
+})
+
+const Login = ({classes}) => {
+    const sttyle = useStyles();
+    return (
+        <Dialog open={true} classes={{paper : classes.dialogg}} BackdropProps={{style : {backgroundColor : 'unset'}}}>
+            <Box className={sttyle.comp}>
+                <Box className={sttyle.lftComp}>
+                    <Typography className={sttyle.title}>To Use WhatsaApp on your computer :</Typography>
+                    <List className={sttyle.listt}>
+                        <ListItem>1. Open WhatsApp On your phone</ListItem>
+                        <ListItem>2. Tap Menu or Settings and select Linked Devices</ListItem>
+                        <ListItem>3. Point your phone to this screen and capture the code</ListItem>
+                    </List>
+                </Box>
+                <Box>
+                    <img src="https://www.ginifab.com/feeds/qr_code/img/qrcode.jpg" alt="qrCode" className={sttyle.qrCode} />
+                </Box>
+            </Box>
+        </Dialog>
+    )
+}
+export default withStyles(style)(Login);
