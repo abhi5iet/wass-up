ae813851aaa3bc7d197d7733e281fe379a2c0bac
Abhishek Verma <77038651+abhi5iet@users.noreply.github.com>
2021-10-05 23:07:24 +0530

    removed errors from front page

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import { Dialog, List, ListItem, Typography, withStyles } from '@mui/material';
+import { Dialog, List, ListItem, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { makeStyles } from '@mui/styled-engine';
+import { makeStyles, withStyles } from '@mui/styles';
 
 const style = {
-    dialogg : {
-        height : '95%',
-        width : '60%',
-        marginTop : '12%',
-        boxShadow : 'none',
+    dialogg: {
+        height: '95%',
+        width: '60%',
+        marginTop: '12%',
+        boxShadow: 'none',
         borderRadius: 0,
-        maxHeight : '100%',
-        maxWidth : '100%',
+        maxHeight: '100%',
+        maxWidth: '100%',
     }
 }
 
 const useStyles = makeStyles({
-    comp : {
+    comp: {
         display: 'flex',
     },
-    lftComp : {
-        padding : '56px 0px 56px 56px',
+    lftComp: {
+        padding: '56px 0px 56px 56px',
     },
-    qrCode : {
-        padding : '50px 0px 0px 50px',
-        height : 264,
-        width : 264
+    qrCode: {
+        padding: '50px 0px 0px 50px',
+        height: 264,
+        width: 264
     },
     title: {
-        marginBottom : 0,
+        marginBottom: 0,
         fontSize: 26,
         fontWeight: 300,
         fontFamily: 'Segoe UI,Helvetica Neue,Helvetica,Lucida Grande,Arial,Ubuntu,Cantarell,Fira Sans,sans-serif',
-        color : '#525252',
+        color: '#525252',
     },
-    listt:{
-        '& > *' : {
+    listt: {
+        '& > *': {
             padding: 0,
             fontSize: 18,
             marginTop: 15,
@@ -45,10 +45,10 @@ const useStyles = makeStyles({
     }
 })
 
-const Login = ({classes}) => {
+const Login = ({ classes }) => {
     const sttyle = useStyles();
     return (
-        <Dialog open={true} classes={{paper : classes.dialogg}} BackdropProps={{style : {backgroundColor : 'unset'}}}>
+        <Dialog open={true} classes={{ paper: classes.dialogg }} BackdropProps={{ style: { backgroundColor: 'unset' } }}>
             <Box className={sttyle.comp}>
                 <Box className={sttyle.lftComp}>
                     <Typography className={sttyle.title}>To Use WhatsaApp on your computer :</Typography>
